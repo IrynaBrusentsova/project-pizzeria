719454a7bc505902cba6ee059655d9546d428297
Iryna Brusentsova <bio200480@gmail.com>
2023-01-23 18:07:51 +0200

    Add method to class Product

--- src/js/script.js
+++ src/js/script.js
@@ -52,16 +52,99 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
-  const app = {
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-    },
-  };
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
 
-  app.init();
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
+      console.log('new Product:', thisProduct)
+    }
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template*/
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+     
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      console.log(thisProduct.element);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      console.log(menuContainer);
+
+      /* add element to menu*/
+      menuContainer.appendChild(thisProduct.element);
+      console.log(menuContainer);
+    }   
+
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log( clickableTrigger);
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+  
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log(activeProduct);
+  
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== thisProduct.element && activeProduct !== null){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+         
+      });
+  
+    }
+
+
+  }
+
+
+const app = {
+  initMenu: function() {
+    const thisApp = this;
+    console.log('thisApp.data:', thisApp.data);
+   
+    for (let productData in thisApp.data.products) {
+      new Product(productData, thisApp.data.products[productData]); 
+    };
+  },
+
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = dataSource;
+  },
+
+  init: function () {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+
+    thisApp.initData();
+
+    thisApp.initMenu();
+  },
+
+
+};
+
+
+app.init();
 }
