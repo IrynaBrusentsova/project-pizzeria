2d5c15f6f8a75ddf359c60dcbbe1e0472cd8cb9a
Iryna Brusentsova <bio200480@gmail.com>
2023-02-15 12:45:33 +0200

    Add file to project

---
+++ src/js/components/Booking.js
@@ -0,0 +1,16 @@
+
+import {select } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+    constructor(element) {
+        const thisBooking = this;
+
+        thisBooking.render();
+        thisBooking.initWidget();
+  
+      }
+
+}
+
+export default Booking;
