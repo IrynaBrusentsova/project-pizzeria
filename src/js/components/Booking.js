639eeaadbec0eee1802144e1eb912534cf7b4dda
Iryna Brusentsova <bio200480@gmail.com>
2023-02-16 17:01:56 +0200

    Change methods render and initWidge in class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 
 import { select, templates } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -16,13 +18,20 @@ class Booking {
     thisBooking.dom = {};
 
     thisBooking.dom.wrapper = element;
-    console.log( thisBooking.dom.wrapper);
+    console.log(thisBooking.dom.wrapper);
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
 
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
 
     thisBooking.dom.houseAmount = document.querySelector(select.booking.hoursAmount);
     console.log(thisBooking.dom.peopleAmount);
+
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    console.log( thisBooking.dom.datePicker);
+
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
+    console.log( thisBooking.dom.hourPicker);
+
   };
 
   initWidget() {
@@ -33,6 +42,12 @@ class Booking {
 
     thisBooking.houseAmount = new AmountWidget(thisBooking.dom.houseAmount);
     thisBooking.dom.houseAmount.addEventListener('update', function () { });
+
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.dom.datePicker.addEventListener('update', function () { });
+
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.dom.hourPicker.addEventListener('update', function () { });
 
   };
 
